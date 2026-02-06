a326603ca0485e87095e2beec94a38aeb94396d4
Merry <svgr1808@outlook.de>
2026-02-06 22:58:31 +0100

    Block teleport requests to ignored players
    Integrated `IgnoreManager` into the teleport command to prevent sending teleport requests to ignored players. Updated translations to include a related error message.

--- src/main/java/net/crystalix/teleport/command/TeleportCommand.java
+++ src/main/java/net/crystalix/teleport/command/TeleportCommand.java
@@ -4,6 +4,7 @@ import net.crystalix.teleport.TeleportPlugin;
 import net.crystalix.teleport.command.cloud.PaperCommand;
 import net.crystalix.teleport.command.cloud.PaperCommandSource;
 import net.crystalix.teleport.command.cloud.PaperPlayerCommandSource;
+import net.crystalix.teleport.util.IgnoreManager;
 import net.crystalix.teleport.util.RequestManager;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
@@ -18,10 +19,12 @@ import static org.incendo.cloud.bukkit.parser.PlayerParser.playerParser;
 public class TeleportCommand extends PaperCommand<TeleportPlugin> {
 
     private final RequestManager requestManager;
+    private final IgnoreManager ignoreManager;
 
     public TeleportCommand(TeleportPlugin plugin) {
         super(plugin);
         this.requestManager = new RequestManager(plugin);
+        this.ignoreManager = plugin.ignoreManager();
     }
 
     @Override
@@ -35,6 +38,10 @@ public class TeleportCommand extends PaperCommand<TeleportPlugin> {
 
                     if (player.equals(target)) {
                         player.sendMessage(translatable("command.request.error.self"));
+                        return;
+                    }
+                    if (ignoreManager.isIgnored(target)) {
+                        player.sendMessage(translatable("command.request.error.ignored"));
                         return;
                     }
 
