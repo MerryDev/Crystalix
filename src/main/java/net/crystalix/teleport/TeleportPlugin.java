2b30384b41c340fe45e72b550de09b63efe725d5
Merry <svgr1808@outlook.de>
2026-02-06 21:23:54 +0100

    Add multi-language support

--- src/main/java/net/crystalix/teleport/TeleportPlugin.java
+++ src/main/java/net/crystalix/teleport/TeleportPlugin.java
@@ -4,6 +4,10 @@ import io.papermc.paper.command.brigadier.CommandSourceStack;
 import net.crystalix.teleport.command.TeleportCommand;
 import net.crystalix.teleport.command.cloud.PaperCommandSource;
 import net.crystalix.teleport.command.cloud.PaperPlayerCommandSource;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.text.minimessage.translation.MiniMessageTranslationStore;
+import net.kyori.adventure.translation.GlobalTranslator;
+import net.kyori.adventure.util.UTF8ResourceBundleControl;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,12 +16,29 @@ import org.incendo.cloud.execution.ExecutionCoordinator;
 import org.incendo.cloud.paper.PaperCommandManager;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class TeleportPlugin extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        createTranslations();
+    }
 
     @Override
     public void onEnable() {
         registerCommand();
         getLogger().info("TeleportPlugin wurde erfolgreich aktiviert");
+    }
+
+    private void createTranslations() {
+        final MiniMessageTranslationStore store = MiniMessageTranslationStore.create(Key.key("magicmail:translations"));
+
+        store.defaultLocale(Locale.GERMAN);
+        store.registerAll(Locale.GERMAN, ResourceBundle.getBundle("messages", Locale.GERMANY, UTF8ResourceBundleControl.utf8ResourceBundleControl()), true);
+
+        GlobalTranslator.translator().addSource(store);
     }
 
     private void registerCommand() {
