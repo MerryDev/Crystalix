972ae942226ae5a73f802a31b98f308cefabd5a9
Merry <56554514+MerryDev@users.noreply.github.com>
2026-02-06 23:16:35 +0100

    Teleport command and toggle option (#1)
    * Implement abstract command base and add Paper-specific implementations
    
    * Integrate PaperCommandManager
    
    * Register TeleportCommand
    
    * Implement simple tpa command
    
    * Add acceptRequest method to handle teleport request acceptance
    
    * Add multi-language support
    
    * Enhance user experience
    
    With this enhancement, clickable chat buttons for accepting and denying were introduced. Also, all messages have been made translatable to support multiple languages
    
    * Add translatable English messages
    
    * Refactor request creation
    
    * Create teleport ignore command
    
    * Add jacksonbukkit dependency
    
    * Add IgnoreManage
    
    Introduced IgnoreManager to manage ignored players with configuration persistence using Jackson. Integrated handling for loading and saving ignored players during plugin lifecycle.
    
    * Add `tpignore` command to toggle teleport request blocking
    
    Introduced a `tpignore` command to allow players to toggle blocking or unblocking of teleport requests. Added German translations for related messages.
    
    * Improve IgnoreManager file handling
    
    Refactor IgnoreManager to dynamically create directories and configuration files if missing. Add exception handling in TeleportPlugin for reading and saving configuration files.
    
    * Improve message bundle
    
    * Block teleport requests to ignored players
    
    Integrated `IgnoreManager` into the teleport command to prevent sending teleport requests to ignored players. Updated translations to include a related error message.
    
    * Add README
    
    * Optimize imports

---
+++ src/main/java/net/crystalix/teleport/util/IgnoreManager.java
@@ -0,0 +1,61 @@
+package net.crystalix.teleport.util;
+
+import net.crystalix.teleport.TeleportPlugin;
+import org.bukkit.entity.Player;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public final class IgnoreManager {
+
+    private final Set<UUID> ignoredPlayers = ConcurrentHashMap.newKeySet();
+    private final TeleportPlugin plugin;
+
+    public IgnoreManager(TeleportPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void ignore(Player player) {
+        ignoredPlayers.add(player.getUniqueId());
+    }
+
+    public void unignore(Player player) {
+        ignoredPlayers.remove(player.getUniqueId());
+    }
+
+    public boolean isIgnored(Player player) {
+        return ignoredPlayers.contains(player.getUniqueId());
+    }
+
+    public void readConfig() throws IOException{
+        Path path = createOrGetPath();
+        Config config = plugin.jsonMapper().readValue(path.toFile(), Config.class);
+
+        ignoredPlayers.clear();
+        ignoredPlayers.addAll(config.uuids());
+    }
+
+    public void saveAll() throws IOException {
+        Path path = createOrGetPath();
+        plugin.jsonMapper().writeValue(path, new Config(new ArrayList<>(ignoredPlayers)));
+    }
+
+    private Path createOrGetPath() throws IOException {
+        final Path directory = plugin.getDataFolder().toPath();
+
+        if (Files.notExists(directory)) {
+            Files.createDirectory(directory);
+        }
+        final Path configPath = directory.resolve("config.json");
+        if (Files.notExists(configPath)) {
+            Files.createFile(configPath);
+        }
+
+        return configPath;
+    }
+}
